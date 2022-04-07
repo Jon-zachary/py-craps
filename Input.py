02e3c356beb3b54c390136873b9bc3d5373af7df
Jon Zachary <jontzachary@gmail.com>
2022-04-07 14:46:57 -0400

    Move IO funcs to Input module

---
+++ Input.py
@@ -0,0 +1,32 @@
+import pickle
+
+class Input():
+  def __init__(self):
+    self.unicode_to_dice = {'1': '\U00012680'}
+
+  def save_game(player):
+    with open('savefile', 'wb') as f:
+      pickle.dump(player, f)
+      print('Game saved')
+
+  def load_game():
+    with open('savefile', 'wb') as f:
+      player = pickle.load(f)
+    return player
+
+  def roll_or_quit(self):
+    player_input = input('press r to roll or e to exit: ')
+    self.check_input(player_input)
+    return player_input
+
+  def check_input(self, player_input):
+    return player_input in ['r', 'e']
+
+  def get_player_input(self):
+    player_input = self.roll_or_quit()
+    while not self.check_input(player_input):
+      player_input = self.roll_or_quit()
+    return player_input
+
+  def print_dice(self):
+    pass
--- main.py
+++ main.py
@@ -1,37 +1,20 @@
 from tabnanny import check
 import Game
+import Input as IO
 
-#TODO create input module
-
-class Input():
-  def roll_or_quit(self):
-    player_input = input('press r to roll or e to exit: ')
-    self.check_input(player_input)
-    return player_input
-
-  def check_input(self, player_input):
-    return player_input in ['r', 'e']
-  
-  def get_player_input(self):
-    player_input = self.roll_or_quit()
-    while not self.check_input(player_input):
-      player_input = self.roll_or_quit()
-    return player_input
-
-# FIX ask player to roll or quit after bet
 def play_game():
-  io = Input()
+  io = IO.Input()
   game = Game.Game()
   game.setup()
   player_input = None
   while player_input == 'r' or player_input == None:
     while not game.point:
-      game.come_out() 
+      game.come_out()
       player_input = io.get_player_input()
       if player_input == 'e':
         exit()
 
-    while game.point: 
+    while game.point:
       game.on_point()
       player_input = io.get_player_input()
       if player_input == 'e':
@@ -40,6 +23,7 @@ def play_game():
   player_input = io.get_player_input()
   if player_input == 'e':
       exit()
+
 
 play_game()
 
