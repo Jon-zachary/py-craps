be1f514c95231a2c3830149f43780b3578b63951
Jon Zachary <jontzachary@gmail.com>
2022-05-08 17:05:58 -0400

    Add interrupt handling

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 from tabnanny import check
 import Game
 import Input as inout
+import sys
 
 def play_game():
-  io = inout.Input()
-  game = Game.Game()
-  game.setup()
-  player_input = None
-  while player_input == 'r' or player_input == None:
-    while not game.point:
-      loading = input("press 's' to save your game")
-      if loading == 's':
-        io.save_game(game.player)
-        
+  try:
+    io = inout.Input()
+    game = Game.Game()
+    game.setup()
+    player_input = None
+    while player_input == 'r' or player_input == None:
+      while not game.point:
+        loading = input("press 's' to save your game")
+        if loading == 's':
+          io.save_game(game.player)
+          
 
-      game.come_out()
-      player_input = io.get_player_input()
-      if player_input == 'e':
+        game.come_out()
+        player_input = io.get_player_input()
+        if player_input == 'e':
+          exit()
+
+      while game.point:
+        game.on_point()
+        player_input = io.get_player_input()
+        if player_input == 'e':
+          exit()
+
+    player_input = io.get_player_input()
+    if player_input == 'e':
         exit()
-
-    while game.point:
-      game.on_point()
-      player_input = io.get_player_input()
-      if player_input == 'e':
-        exit()
-
-  player_input = io.get_player_input()
-  if player_input == 'e':
-      exit()
+  except(KeyboardInterrupt):
+    print('\n Game Over')
+    sys.exit(0)
 
 
 play_game()
